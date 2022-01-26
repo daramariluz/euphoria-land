60f18199004715a0433dc2fd5afc5d4d189a92b7
darahancco <darahancco@gmail.com>
2022-01-26 14:31:37 -0500

    frontend banner cards etcc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_213301) do
+ActiveRecord::Schema.define(version: 2022_01_26_190748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2022_01_25_213301) do
     t.string "model"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   add_foreign_key "bookings", "users"
